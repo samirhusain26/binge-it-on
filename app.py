d4144f014185cec883a33ea4fbe76d1dd49d4e4e
Samir Husain <samir.husain26@gmail.com>
2020-05-04 23:00:45 -0400

    Push data

---
+++ app.py
@@ -0,0 +1,61 @@
+import flask
+import difflib
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import requests
+import pandas as pd
+
+api_key = 'e5e135ca8f4f2ecfe0a67c9998a5441a'
+
+app = flask.Flask(__name__, template_folder='templates')
+
+df2 = pd.read_csv('./model/tmdb.csv')
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(df2['soup'])
+cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
+df2 = df2.reset_index()
+indices = pd.Series(df2.index, index=df2['title'])
+all_titles = [df2['title'][i] for i in range(len(df2['title']))]
+
+def get_recommendations(title):
+    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
+    tit = df2['title'].iloc[movie_indices]
+    dat = df2['release_date'].iloc[movie_indices]
+    return_df = pd.DataFrame(columns=['Title','Year'])
+    return_df['Title'] = tit
+    return_df['Year'] = dat
+    return return_df
+
+# Set up the main route
+@app.route('/', methods=['GET', 'POST'])
+
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('index.html'))
+            
+    if flask.request.method == 'POST':
+        yr = flask.request.form['year']
+        rate = flask.request.form['rating']
+        gen = flask.request.form['genre']
+        response = requests.get('https://api.themoviedb.org/3/discover/movie?api_key=' + api_key
+                                + '&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1'
+                                  '&primary_release_date.gte=' + yr
+                                + '&vote_average.gte=' + rate
+                                + '&with_genres=' + gen)
+        result = response.json()
+        movies = result['results']
+        names=[]
+        dates=[]
+        for film in movies:
+            names.append(film['title'])
+            dates.append(film['release_date'])
+        return flask.render_template('positive.html',movie_names=names,movie_date=dates,search_name='Hello')
+
+if __name__ == '__main__':
+    app.run()
